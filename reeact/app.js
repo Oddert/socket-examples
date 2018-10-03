8898f86a27289512c9b14823b09057ebe74fb10a
Robyn <oddert@gmail.com>
2018-10-03 13:30:03 +0100

    added react example

--- reeact/app.js
+++ reeact/app.js
@@ -1,16 +1,31 @@
-const express = require('express')
-    , app = express()
-    , bodyParser = require('body-parser')
-    , cookieParser = require('cookie-parser')
+const express       = require('express')
+    , app           = express()
+    , bodyParser    = require('body-parser')
+    , cookieParser  = require('cookie-parser')
+    , path          = require('path')
+    , socket        = require('socket.io')
 
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.route('/api/test').get((req, res) => res.json({ message: `server @ res ok` }))
 
 const PORT = process.env.PORT || 5000
-app.listen(
+var server = app.listen(
   PORT,
   () => console.log(`${new Date().toLocaleTimeString()}: Server initialising on PORT: ${PORT}...`)
 )
+
+const io = socket(server)
+
+io.on('connection', socket => {
+  console.log(`A user connected: ${socket.client.id}`)
+  const counter = setInterval(() => {
+    socket.emit('newItem', `Server created: ${Math.floor(Math.random() * 30)}`)
+  }, 5000)
+  socket.on('userAdd', payload => {
+    console.log(payload)
+    socket.broadcast.emit('newItem', `User ${socket.client.id} added: ${payload}`)
+    socket.emit('newItem', `User ${socket.client.id} added: ${payload}`)
+  })
+})
