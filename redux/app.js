e82715cc33239f7517bad4e115b8cf9010b27dab
Robyn <oddert@gmail.com>
2018-10-03 18:58:45 +0100

    fixed missing bracket

--- redux/app.js
+++ redux/app.js
@@ -8,7 +8,7 @@ const express = require('express')
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, '/client/build'))
+app.use(express.static(path.join(__dirname, '/client/build')))
 
 const PORT = process.env.PORT || 5000
 var server = app.listen(
